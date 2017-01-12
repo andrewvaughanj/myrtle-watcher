8a17c97b52bf9818d37e236b71145f4b99bc76c4
Andrew V. Jones <avj@vectorcast.com>
2017-01-12 08:38:30 +0000

    AVJ: wrapping animate to work around the broken images bug

--- animate.py
+++ animate.py
@@ -12,7 +12,6 @@ if not os.path.exists(VIDEOS_DIR):
     os.makedirs(VIDEOS_DIR)
 
 try:
-
     # Double of bunnies, because capturing at 30 seconds
     framerate = 20
 
@@ -20,8 +19,12 @@ try:
     # os.system("ffmpeg -y -framerate 10 -pattern_type glob -i '{}/2*.jpg' -pix_fmt rgb8 -ignore_loop 0 -s 576x384 {}/animated.gif".format(CAPTURES_DIR, CAPTURES_DIR))
 
     # Create .mp4 for YouTube
-    os.system("ffmpeg -y -framerate {} -pattern_type glob -i '{}/2*.jpg' -vcodec mpeg4 -q:v 10 {}/animated.mp4".format(
-        framerate, CAPTURES_DIR, CAPTURES_DIR))
+    # os.system("ffmpeg -y -framerate {} -pattern_type glob -i '{}/2*.jpg' -vcodec mpeg4 -q:v 10 {}/animated.mp4".format(
+    #    framerate, CAPTURES_DIR, CAPTURES_DIR))
+    TEMP_DIR = os.path.join(MYRTLE_WATCHER_DIR, "static/temp")
+    ffmpeg_command = "ffmpeg -y -i {}/\"frame_%04d.jpg\" -framerate {} -c:v libx264 -pix_fmt yuv420p -preset faster {}/animated.mp4"
+    ffmpeg_command = ffmpeg_command.format(TEMP_DIR, framerate, CAPTURES_DIR)
+    os.system(ffmpeg_command)
 
     # Copy the mp4 to the back-up location
     curr_time = datetime.datetime.now().isoformat().replace(".", "_").replace(":", "_")
---
+++ animate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm -rf "static/temp"
+mkdir "static/temp"
+cd "static/temp"
+for f in `ls -rt ../captures/2*.jpg`
+do
+    # get the index in 0000 format
+    printf -v counts "%04d" $count
+
+    # check if the file exists
+    if [ -s "$f" ]
+    then
+        # if it does, copy it and link it
+        cp $f "frame_$counts.jpg"
+        # increment counter
+        count=`expr $count + 1`
+    fi
+    done
+cd ../..
+
+/home/pi/myrtle-watcher/animate.py
+
+rm -rf "static/temp"
